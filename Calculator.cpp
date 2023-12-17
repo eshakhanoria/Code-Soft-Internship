1f35b07a8e5e53d6f859f1b1f8706ef8e3dec400
eshakhanoria <142670121+eshakhanoria@users.noreply.github.com>
2023-12-17 13:13:34 +0530

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double calculate(double Number1, double Number2, char operation)
+{
+    switch (operation)
+    {
+    case '+':
+        cout << " Result : " << Number1 + Number2;
+        break;
+    case '-':
+        cout << " Result : " << Number1 - Number2;
+        break;
+    case '*':
+        cout << " Result : " << Number1 * Number2;
+        break;
+    case '/':
+        if (Number2 == 0)
+        {
+            cout << " Division invalid " << endl;
+        }
+        else
+        {
+            cout << " Result : " << Number1 / Number2;
+        }
+        break;
+    default:
+        cout << "Operation is not valid";
+    }
+}
+
+int main()
+{
+    double Number1, Number2;
+    char operation;
+
+    cout << " Enter Number 1 :  ";
+    cin >> Number1;
+    cout << " Enter Number 2 :  ";
+    cin >> Number2;
+    cout << " Operation to Carry ->(+,-,*,/) : ";
+    cin >> operation;
+
+    calculate(Number1, Number2, operation);
+    return 0;
+}
